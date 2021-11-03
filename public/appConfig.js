fc783d06873d8cd21e4d88e8265885be27eeab37
John robert <86650219+Deeksha11alva@users.noreply.github.com>
2021-11-03 21:48:23 +0530

    Add files via upload

---
+++ public/appConfig.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+    var appConfig = {
+
+
+        serviceBaseUrl: "https://dancing-owl-1234.twil.io/",
+     
+          accountSid: 'AC8a74265a424f710f6863781ce45f7352',
+      
+    pluginService: {
+      enabled: true,
+      url: '/plugins',
+    },
+    ytica: false,
+    logLevel: 'info',
+    showSupervisorDesktopView: true,
+  };
+  
